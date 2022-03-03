a63021db68022d36a60fc804f7912c734958e1eb
MohammadAbdullahkhan1 <100206902+MohammadAbdullahkhan1@users.noreply.github.com>
2022-03-03 19:21:58 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,62 +1,58 @@
 #include "stats.h"
 #include <math.h>
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
-
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
-    int i = 0;
-    float sum = 0;
-    float max = 0;
-    float min = 0 ;
-    if(numberset != 0)
+    int i;
+    float sum=0.0,max,min;
+    
+    if(setlength>0)
     {
-       max = *(numberset+0);
-       min = *(numberset+0);
-       for(i=0; i< setlength; i++)
+        /* maximum in the given number set */
+        maximum=numberset[0];
+        for(i=1;i<setlength;i++)
         {
-            (*(numberset+i)>max)?(max = *(numberset+i)):(min = *(numberset+i));
-            sum = sum+*(numberset+i);
+		    (numberset[i]>max)?(max=numberset[i]):(min=numberset[i]);
+
         }
-          sum = sum + *(numberset+i);
-       }
-      s.average = sum/setlength;
-      s.max = max;
-      s.min = min;
+        s.min=minimum;
+        s.max=maximum;
+    
+        /* Finding Average of the given number set */
+        for(i=0;i<setlength;i++)
+        {
+            sum+=numberset[i];
+         }
+        s.average=sum/setlength;
     }
     else
     {
-     s.average = 0;
-     s.max = 0;
-     s.min = 0;
+        s.min=NAN;
+        s.max=NAN;
+        s.average=NAN;
     }    
-    return s;
+     return s;
 }
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 void emailAlerter()
 {
-   emailAlertCallCount++;
+    emailAlertCallCount++;
 }
 void ledAlerter()
 {
-  ledAlertCallCount++;
+    ledAlertCallCount++;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
-   if(computedStats.max > maxThreshold)
-   { 
-     if(0 != alerters[0])
-     {
-        alerters[0](); 
-     }
-     if(0 != alerters[1])
-     {
-        alerters[1](); 
-     }
+    int maximum= computedStats.max;
+    if(maxThreshold < maximum)
+    {
+        (*alerters[0])();
+        (*alerters[1])();
     }
-}
+}  
